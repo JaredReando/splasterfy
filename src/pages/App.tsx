b64e5515890b64a6a1cf5d4c7294d74b70ea9eea
Jared Reando <jared.reando@streem.pro>
2020-09-08 23:28:25 -0700

    Get last.fm request working.

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,14 +2,23 @@ import React, {useState} from 'react';
 
 import './App.css';
 
+const BASE_LAST_FM_URL = "http://ws.audioscrobbler.com/2.0"
+const getSimiliarArtist = async (artist: string) => {
+  const requestUrl = BASE_LAST_FM_URL + `?method=artist.getsimilar&artist=${encodeURI(artist)}&api_key=${process.env.REACT_APP_LAST_FM_KEY}&format=json`
+  const response = await fetch(requestUrl)
+    .then(res => res.json())
+  return response;
+}
+
 function App() {
   const [input, setInput] = useState('')
   return (
     <div className="App">
       <header className="App-header">
-        <form onSubmit={(e: React.SyntheticEvent<HTMLFormElement>) => {
-            e.preventDefault()
-            console.log("form data submitted: ", input)
+        <form onSubmit={async (e: React.SyntheticEvent<HTMLFormElement>) => {
+          e.preventDefault()
+          const search = await getSimiliarArtist(input)
+          console.log("url: ", search)
         }}>
           <label htmlFor='artist'>Artist</label>
           <input 
