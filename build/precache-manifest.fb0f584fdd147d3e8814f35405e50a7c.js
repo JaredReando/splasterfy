a415bb7588e3b88e36e70d274415ee8c28ac17f2
Jared Reando <43949038+JaredReando@users.noreply.github.com>
2022-09-13 10:16:50 -0700

    Add build directory to git.

---
+++ build/precache-manifest.fb0f584fdd147d3e8814f35405e50a7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf1e28dfcbd45514c54bf46fb55990d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c729d65532fef61ecb1",
+    "url": "/static/css/main.6b113088.chunk.css"
+  },
+  {
+    "revision": "52fde819dc584a034da6",
+    "url": "/static/js/2.85289691.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/static/js/2.85289691.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c729d65532fef61ecb1",
+    "url": "/static/js/main.96ed5dcb.chunk.js"
+  },
+  {
+    "revision": "8fe741621f21731977a6",
+    "url": "/static/js/runtime-main.34f5cfbc.js"
+  }
+]);
